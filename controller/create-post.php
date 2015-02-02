f9b570fd2e67dd918837d0030e9aa541498e5426
Roger Pirrone <r.j.pirrone.4@gmail.com>
2015-02-02 12:14:30 -0800

    finished video 27

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,12 +1,18 @@
 <?php
-    require_once(__DIR__ . "/../model/database.php");
+    require_once(__DIR__ . "/../model/config.php");
     
     $connection = new mysqli($host, $username, $password, $database);
 
     $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
     $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
     
-    echo "<p>Title: $title</p>";
-    echo "<p>Post: $post</p>";
+    $query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post' ");
+    
+    if($query) {
+        echo "<p>Successfully Inserted Post: $title</p>";
+    }
+    else {
+        echo "<p>$connection->error</p>";
+    }
     
     $connection->close();
