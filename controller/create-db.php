1b8669a2f479bfb790469efb40ea20e4cd2e6c6c
Roger Pirrone <r.j.pirrone.4@gmail.com>
2015-02-03 11:05:57 -0800

    went up to video 33

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,24 +1,5 @@
 <?php
     require_once(__DIR__ . "/../model/config.php");
-
-    $connection = new mysqli($host, $username, $password);
-    
-    if($connection->connect_error){
-        die("<p>Error: " . $connection->connect_error . "</p>");
-    }
-
-    $exists = $connection->select_db($database);
-    
-    if(!$exists) {
-        $query = $connection->query("CREATE DATABASE $database");
-        
-        if($query) {
-            echo "<p>Successfully created database " . $database . "</p>";
-        }
-    }
-    else {
-        echo "<p>Database already exists.</p>";
-    }
     
     $query = $connection->query("CREATE TABLE posts ("
             
@@ -35,4 +16,3 @@
     }
     
     
-    $connection->close();
--- model/Database.php
+++ model/Database.php
@@ -11,5 +11,49 @@ class Database {
         $this->username = $username;
         $this->passoword = $password;
         $this->database = $database;
+        
+        $this->connection = new mysqli($host, $username, $password);
+
+        if ($this->connection->connect_error) {
+            die("<p>Error: " . $this->connection->connect_error . "</p>");
+        }
+
+        $exists = $this->connection->select_db($database);
+
+        if (!$exists) {
+            $query = $this->connection->query("CREATE DATABASE $database");
+
+            if ($query) {
+                echo "<p>Successfully created database " . $database . "</p>";
+            }
+        } else {
+            echo "<p>Database already exists.</p>";
+        }
     }
+    
+    public function openConnection() {
+        $this->connection = new mysqli($this->$host, $this->username, $this->password, $this->database);
+        
+        if ($this->$connection->connect_error) {
+            die("<p>Error: " . $this->connection->connect_error . "</p>");
+        }
+    }
+    
+    public function closeConnection() {
+        if(isset($this->connection)) {
+            $this->connection->close();
+        }
+    }
+    
+    public function query($string) {
+        $this->openConnection();
+        
+        $query = $this->connection->query($string);
+        
+        
+        $this->closeConnection();
+        
+        return $query;
+    }
+    
 }
