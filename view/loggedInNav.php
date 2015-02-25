a5cdef015b445991afd77d135a16181b0973ef03
Roger Pirrone <r.j.pirrone.4@gmail.com>
2015-02-25 11:19:56 -0800

    ddddddddddddddddddddddddddddddiiiiiiiiiiiiiiiiiiiiiidddddddddddddddddddd sssssssssssssssssssssooooooooooooooooommmmmmmmmmmmmmmeeeeeeeeeeeeeeeee ssssssssssssssssttttttttttttttttuuuuuuuuuuuuuuuuufffffffffffffffffffffffffffffffffffffffff

--- view/loggedInNav.php
+++ view/loggedInNav.php
@@ -1,8 +1,9 @@
-        <!--this is for the nav bar-->
-<li role="presentation"><a role="menuitem" tabindex="-1" href="index.php">Home</a></li>
+        <!--this is for the nav bar-->        
+        <ul id="animateList"><br><li role="presentation"><a role="menuitem" tabindex="-1" href="index.php">Home</a></li>
 
-<li role="presentation"><a role="menuitem" tabindex="-1" href="controller/logout-user.php">Logout</a></li>
+        <li role="presentation"><a role="menuitem" tabindex="-1" href="controller/logout-user.php">Logout</a></li>
 
-<li role="presentation"><a role="menuitem" tabindex="-1" href="index.html">Portfolio</a></li>
+        <li role="presentation"><a role="menuitem" tabindex="-1" href="index.html">Portfolio</a></li>
 
-<li><a href="<?php echo $path . "post.php"?>">Post</a></li>
+        <li><a href="<?php echo $path . "post.php"?>">Post</a></li>
+        </ul>
--- view/loggedOutNav.php
+++ view/loggedOutNav.php
@@ -1,8 +1,10 @@
         <!--this is for the nav bar-->
-<li role="presentation"><a role="menuitem" tabindex="-1" href="index.php">Home</a></li>
+        
+        <ul id="animateList"><li role="presentation"><a role="menuitem" tabindex="-1" href="index.php">Home</a></li>
 
 <li role="presentation"><a role="menuitem" tabindex="-1" href="login.php">Login</a></li>
         
 <li role="presentation"><a role="menuitem" tabindex="-1" href="register.php">Register</a></li>
 
 <li role="presentation"><a role="menuitem" tabindex="-1" href="index.html">Portfolio</a></li>
+        </ul>
